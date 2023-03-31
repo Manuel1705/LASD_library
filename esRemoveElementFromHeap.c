ecceeff2abab9bc277024a1f6aa9db65ed818f6f
Manuel1705 <manuelmignogna5@gmail.com>
2023-03-31 18:07:45 +0200

    commit

--- esRemoveElementFromHeap.c
+++ esRemoveElementFromHeap.c
@@ -2,25 +2,27 @@
 // scrivere una procedura che, in tempo logaritmico,
 // presi in input H e un valore numerico v presente in H(con relativo indice di posizione di v nel vettore),
 // rimuova v da H, mantenendo la proprietà di heap.
-
+// IMPORTANTE
+// ARRAY[0] CONTIENE LA DIMENSIONE DELL'HEAP
+// L'algoritmo funziona ma deleteElementHeap può essere ottimizata
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define MAX 15
-
+#define MAX 16
+// calcolo figlio sinistro con indice che parte da 1
 int left(int parent)
+{
+    return 2 * parent;
+}
+// calcolo figlio destro con indice che parte da 1
+int right(int parent)
 {
     return 2 * parent + 1;
 }
-
-int right(int parent)
-{
-    return 2 * parent + 2;
-}
-
+// calcolo padre con indice che parte da 1
 int parent(int son)
 {
-    return (son - 1) / 2;
+    return son / 2;
 }
 void swap(int *array, int a, int b)
 {
@@ -35,10 +37,10 @@ void heapify(int *array, int dim, int parent)
     int r = right(parent);
     int max = parent;
 
-    if (l < dim && array[max] <= array[l])
+    if (l <= dim && array[max] <= array[l])
         max = l;
 
-    if (r < dim && array[max] <= array[r])
+    if (r <= dim && array[max] <= array[r])
         max = r;
 
     if (max != parent)
@@ -50,14 +52,15 @@ void heapify(int *array, int dim, int parent)
 
 void buildHeap(int *array, int dim)
 {
-    for (int i = dim / 2 - 1; i >= 0; i--)
+    for (int i = dim / 2; i > 0; i--)
         heapify(array, dim, i);
 }
 
 void printHeap(int *heap, int dim)
 {
+    printf("\nHeap (dim %d):", dim);
     int nextLevel = 1;
-    for (int i = 0; i < dim; i++)
+    for (int i = 1; i <= dim; i++)
     {
         if (i == nextLevel)
         {
@@ -70,14 +73,30 @@ void printHeap(int *heap, int dim)
 }
 int getIndex(int *array, int dim, int element)
 {
-    for (int i = 0; i < dim; i++)
+    for (int i = 1; i <= dim; i++)
         if (array[i] == element)
             return i;
-    printf("The element is not stored in the array");
+    printf("\nThe element is not stored in the array\n");
     return -1;
 }
-void deleteElementHeap(int *array, int dim, int index)
+int checkStored(int *array, int dim, int element)
 {
+    for (int i = 1; i <= dim; i++)
+        if (array[i] == element)
+            return 1;
+    return 0;
+}
+void repairHeap(int *array, int parent, int dim)
+{
+}
+void deleteElementHeap(int *heap, int dim, int index)
+{
+    if (index >= 1 && index <= heap[0])
+    {
+        swap(heap, index, heap[0]);
+        heap[0]--;
+        buildHeap(heap, heap[0]);
+    }
 }
 int main()
 {
@@ -88,19 +107,27 @@ int main()
     int dim;
     do
     {
-        printf("Enter the heap dimention(max: %d): ", MAX);
+        printf("\nEnter the heap dimention(max: %d): ", MAX - 1);
         scanf("%d", &dim);
     } while (dim > MAX && dim <= 0);
 
-    for (int i = 0; i < dim; i++)
-        heap[i] = (rand() % 100) + 1;
+    for (int i = 1; i <= dim; i++)
+    {
+        int d;
+        do
+        {
+            d = (rand() % 100) + 1;
+            heap[i] = d;
+        } while (checkStored(heap, heap[0], d));
+        heap[0]++;
+    }
 
-    buildHeap(heap, dim);
-    printHeap(heap, dim);
+    buildHeap(heap, heap[0]);
+    printHeap(heap, heap[0]);
 
     int d;
     printf("Enter the element to delete from the heap: ");
     scanf("%d", &d);
-    int index = getIndex(heap, dim, d);
-    deleteElementHeap(heap, dim, index);
+    deleteElementHeap(heap, dim, getIndex(heap, dim, d));
+    printHeap(heap, heap[0]);
 }
